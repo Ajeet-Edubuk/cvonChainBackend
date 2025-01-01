a69e03ce1afffa85c1a78b9b0f754bc8627da2b0
ajeetram <86039147+ajeetram@users.noreply.github.com>
2025-01-01 14:27:12 +0530

    edubuk-okto-backend

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@ config();
 app.use(
   cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: ['https://www.edubukcvonchain.com',
-  'https://edubukcvonchain.com',],
+    origin: [
+  'https://edubukcvonchain.com'],
     credentials: true,
   })
 );
